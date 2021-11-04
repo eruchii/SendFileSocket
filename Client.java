5b791e470280faebc28ed5b704130f9134ce8088
eruchii <nct1120@gmail.com>
2021-11-04 09:07:41 +0700

    done

--- Client.java
+++ Client.java
@@ -7,22 +7,25 @@
 
 import java.io.*;
 import java.net.*;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class Client {
 
     public static void main(String[] args) {
-
-        final String serverHost = "localhost";
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhap IP: ");
+        String serverHost = sc.nextLine();
 
         Socket socketOfClient = null;
-        DataOutputStream os = null;
-        DataInputStream is = null;
+        OutputStream os = null;
+        InputStream is = null;
+        byte[] buff = new byte[4096];
 
         try {
             socketOfClient = new Socket(serverHost, 9999);
-            os = new DataOutputStream(new BufferedOutputStream(socketOfClient.getOutputStream()));
-            is = new DataInputStream(new BufferedInputStream(socketOfClient.getInputStream()));
+            os = socketOfClient.getOutputStream();
+            is = socketOfClient.getInputStream();
 
         } catch (UnknownHostException e) {
             System.err.println("Don't know about host " + serverHost);
@@ -32,41 +35,63 @@ public class Client {
             return;
         }
         System.err.println("Connected to " + serverHost);
-        Scanner sc = new Scanner(System.in);
+        
+        boolean ready = false;
         try {
             while (true) {
-                String input = sc.nextLine();
-                os.writeChars(input);
-                os.writeChar('\n');
-                os.flush();
-                String resp = is.readUTF();
+                if(!ready){
+                    os.write("HELO Server".getBytes());
+                    os.flush();
+                }
+                else{
+                    String input = sc.nextLine();
+                    os.write(input.getBytes());
+                    os.flush();
+                }
+                int recv_bytes = is.read(buff);
+                String resp = new String(buff, StandardCharsets.UTF_8).substring(0, recv_bytes);
                 System.out.println("FROM SERVER: " + resp);
                 if (resp.equals("500 bye")) {
                     break;
                 }
-                if (resp.equals("210 File Info OK")) {
+                if(resp.equals("200 Hello Client")){
+                    ready = true;
+                }
+                if (resp.equals("210 Download Mode OK")) {
+                    System.out.print("Nhap ten file: ");
                     String x = sc.nextLine();
-                    os.writeChars(x+"\n");                
+                    os.write(x.getBytes());                
                     os.flush();
-                    String file_info = is.readUTF();
+                    recv_bytes = is.read(buff);
+                    String file_info = new String(buff, StandardCharsets.UTF_8).substring(0, recv_bytes);
+                    if(file_info.equals("410 File Not Found")){
+                        System.out.println(file_info);
+                        continue;
+                    }
                     FileInfo fi = new FileInfo(file_info);
-                    System.out.println(fi.getFileName());
-                    System.out.println(fi.getFileSize());
+                    System.out.println("Ten file: " + fi.getFileName());
+                    System.out.println("Kich thuoc: " + fi.getFileSize());
                     OutputStream outputFile = new FileOutputStream("download/"+fi.getFileName());
                     int count = 0;
                     int remain = fi.getFileSize();
+                    os.write("DOWNLOAD".getBytes());                
+                    os.flush();
                     byte[] buffer = new byte[4096]; // or 4096, or more
                     while (true) {
+                        if(remain <= 0){
+                            System.out.println("Ket thuc");
+                            break;
+                        }
+                        // System.out.println("Reading");
                         count = is.read(buffer);
+                        // System.out.println(count);
                         if(count <= 0){
                             break;
                         }
                         outputFile.write(buffer, 0, count);
+                        outputFile.flush();
                         remain -= count;
-                        if(remain <= 0){
-                            System.out.println("Finished");
-                            break;
-                        }
+                        // System.out.println(remain);
                     }
                     outputFile.close();
                 }
--- Server.java
+++ Server.java
@@ -9,14 +9,17 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.CharBuffer;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.attribute.BasicFileAttributes;
@@ -28,17 +31,15 @@ public class Server {
 
         ServerSocket listener = null;
         String line;
-        BufferedReader is;
-        OutputStreamWriter os;
+        InputStream is;
+        OutputStream os;
         Socket socketOfServer = null;
 
-        final String INFO_INPUT = "210 OK\n";
-        final String FILE_NOT_FOUND = "400 File Not Found\n";
-        final String INFO_OK = "210 File Info OK\n";
-        final String DOWNLOAD_OK = "220 Download OK\n";
-        final String QUIT = "500 bye\n";
-        final String HELLO = "200 Hello Client\n";
-        final String COMMAND_NOT_FOUND = "400 Command not found\n";
+        final String FILE_NOT_FOUND = "410 File Not Found";
+        final String DOWNLOAD_OK = "210 Download Mode OK";
+        final String QUIT = "500 bye";
+        final String HELLO = "200 Hello Client";
+        final String COMMAND_NOT_FOUND = "400 Command not found";
 
         try {
             listener = new ServerSocket(9999);
@@ -46,59 +47,72 @@ public class Server {
             System.out.println(e);
             System.exit(1);
         }
-
         try {
             System.out.println("Server is waiting to accept user...");
             socketOfServer = listener.accept();
             System.out.println("Accept a client!");
 
-            is = new BufferedReader(new InputStreamReader(socketOfServer.getInputStream()));
-            os = new OutputStreamWriter(socketOfServer.getOutputStream());
+            is = socketOfServer.getInputStream();
+            os = socketOfServer.getOutputStream();
 
             while (true) {
-                line = is.readLine();
-                if (line == null) {
+                byte[] buff = new byte[4096];
+                int cc = is.read(buff);
+                if(cc < 0){
                     break;
                 }
-                
+                line = new String(buff, StandardCharsets.UTF_8).substring(0, cc);                
                 if (line.equals("QUIT")) {
                     System.out.println("client say quit");
-                    os.write(QUIT);
+                    os.write(QUIT.getBytes());
                     os.flush();
                     break;
                 }
                 if(line.equals("HELO Server")){
-                    os.write(HELLO);
+                    os.write(HELLO.getBytes());
                     os.flush();
                     continue;
                 }
-                if (line.equals("FILE INFO")) {
-                    os.write(INFO_OK);
+                if (line.equals("FILE")) {
+                    os.write(DOWNLOAD_OK.getBytes());
                     os.flush();
-                    String fileName = is.readLine();
+                    int re = is.read(buff);
+                    String fileName = new String(buff, StandardCharsets.UTF_8).substring(0, re);
                     System.out.println(fileName);
                     if (fileName == null) {
                         break;
                     }
                     File file = new File(fileName);
                     if(!file.exists()){
-                        os.write(FILE_NOT_FOUND);
+                        os.write(FILE_NOT_FOUND.getBytes());
                         os.flush();
                         continue;
                     }
                     FileInfo fi = new FileInfo(fileName, (int) file.length());
-                    os.write(fi.toString());
-                    os.write("\n");
+                    os.write((fi.toString()+"\n").getBytes());
                     os.flush();
-                    byte[] bytes = new byte[4096];
-                    FileInputStream f = new FileInputStream(file);
-                    int count;
-                    while ((count = f.read(bytes)) > 0) {
-                        os.write(0);
+                    re = is.read(buff);
+                    String nextcmd = new String(buff, StandardCharsets.UTF_8).substring(0, re);
+                    if(!nextcmd.equals("DOWNLOAD")){
+                        continue;
                     }
+                    System.out.println(nextcmd);
+                    byte[] bytes = new byte[4096];
+                    InputStream f = new FileInputStream(fileName);
+                    int count;
+                    while (true) {
+                        count = f.read(bytes);
+                        // System.out.println(count);
+                        if(count < 0){
+                            break;
+                        }
+                        os.write(bytes,0,count);
+                        os.flush();
+                    }
+                    f.close();
                     continue;
                 }
-                os.write(COMMAND_NOT_FOUND);
+                os.write(COMMAND_NOT_FOUND.getBytes());
                 os.flush();
             }
 
